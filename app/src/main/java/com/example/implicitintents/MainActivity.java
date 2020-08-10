f1c699d7097988c9de5474d4a58c4be139052bc1
PhoenixWings7 <adamus.klaudia.pasp@gmail.com>
2020-08-10 16:24:39 +0200

    basic view and first function implementation

---
+++ app/src/main/java/com/example/implicitintents/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.implicitintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openWebsite(View view) {
+        EditText websiteEditText = findViewById(R.id.website_edittext);
+        String webpage = websiteEditText.getText().toString();
+        Uri uri = Uri.parse(webpage);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
+    }
+}
